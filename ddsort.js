79fcfa1e71290f85a6ec961bfad68558acef36e0
Barrior <Barrior@qq.com>
2015-10-28 17:02:09 +0800

    Barrior

---
+++ ddsort.js
@@ -0,0 +1,115 @@
+;(function( $ ){
+	/**
+	 * Author: https://github.com/Barrior
+	 * 
+	 * DDSort: drag and drop sorting.
+	 * @param {Object} options
+	 *        target[string]: 		jQuery事件委托选择器字符串
+	 *        cloneStyle[object]: 	可选，设置占位符元素的样式
+	 *        floatStyle[object]: 	可选，设置拖动元素的样式
+	 *        down[function]: 		可选，鼠标按下时执行的函数
+	 *        move[function]: 		可选，鼠标移动时执行的函数
+	 *        up[function]: 		可选，鼠标抬起时执行的函数
+	 */
+	$.fn.DDSort = function( options ){
+		var $doc = $( document ),
+			fnEmpty = function(){},
+			fnDown = options.down || fnEmpty,
+			fnMove = options.move || fnEmpty,
+			fnUp = options.up || fnEmpty,
+			
+			cloneStyle = $.extend({
+				
+				'background-color': '#eee'
+				
+			}, options.cloneStyle ),
+			
+			floatStyle = $.extend({
+				
+				'position': 'absolute',
+				'box-shadow': '10px 10px 20px 0 #eee',
+				'webkitTransform': 'rotate(4deg)',
+				'mozTransform': 'rotate(4deg)',
+				'msTransform': 'rotate(4deg)',
+				'transform': 'rotate(4deg)'
+				
+			}, options.floatStyle ),
+
+			height = 'height',
+			width = 'width';
+
+		if( this.css( 'box-sizing' ) == 'border-box' ){
+			height = 'outerHeight';
+			width = 'outerWidth';
+		}
+
+		return this.on( 'mousedown.DDSort', options.target, function( e ){
+			var tagName = e.target.tagName.toLowerCase();
+			if( tagName == 'input' || tagName == 'textarea' || tagName == 'select' ){
+				return;
+			}
+
+			var THIS = this,
+				$this = $( THIS ),
+				offset = $this.offset(),
+				disX = e.pageX - offset.left,
+				disY = e.pageY - offset.top,
+			
+				clone = $this.clone()
+					.css( cloneStyle )
+					.css( 'height', $this[ height ]() )
+					.empty(),
+					
+				hasClone = 1;
+			
+			fnDown.call( THIS );
+			
+			$doc.on( 'mousemove.DDSort', function( e ){
+				if( hasClone ){
+					$this.before( clone )
+						.css( 'width', $this[ width ]() )
+						.css( floatStyle )
+						.appendTo( $this.parent() );
+						
+					hasClone = 0;
+				}
+				
+				var left = e.pageX - disX,
+					top = e.pageY - disY,
+					
+					prev = clone.prev(),
+					next = clone.next();
+				
+				$this.css({
+					left: left,
+					top: top
+				});
+				
+				if( prev.length && top < prev.offset().top + prev.outerHeight()/2 ){
+						
+					clone.after( prev );
+					
+				}else if( next.length && top > next.offset().top - next.outerHeight()/2 ){
+					
+					clone.before( next );
+				}
+				
+				fnMove.call( THIS );
+			});
+			
+			$doc.on( 'mouseup.DDSort', function(){
+				
+				$doc.off( 'mousemove.DDSort mouseup.DDSort' );
+				
+				//click的时候也会触发mouseup事件，加上判断阻止这种情况
+				if( !hasClone ){
+					clone.before( $this.removeAttr( 'style' ) ).remove();
+					fnUp.call( THIS );
+				}
+			});
+			
+			return false;
+		});
+	};
+
+})( jQuery );
