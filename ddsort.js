4625c9321b7a22138f7635107840cdec9df873be
Barrior <barrior@qq.com>
2015-12-18 16:32:34 +0800

    beautiful modification
    多处细节修改

--- ddsort.js
+++ ddsort.js
@@ -12,150 +12,152 @@
 	 *        up[function]: 		可选，鼠标抬起时执行的函数
 	 */
 	$.fn.DDSort = function( options ){
-		var that = this,
-			$doc = $( document ),
+		var $doc = $( document ),
 			fnEmpty = function(){},
-			options = options || {},
-			fnDown = options.down || fnEmpty,
-			fnMove = options.move || fnEmpty,
-			fnUp = options.up || fnEmpty,
-			
-			cloneStyle = $.extend({
-				
-				'background-color': '#eee'
-				
-			}, options.cloneStyle ),
-			
-			floatStyle = $.extend({
-				
-				//用固定定位可以防止定位父级不是Body的情况的兼容处理，表示不兼容IE6，无妨
-				'position': 'fixed',
-				'box-shadow': '10px 10px 20px 0 #eee',
-				'webkitTransform': 'rotate(4deg)',
-				'mozTransform': 'rotate(4deg)',
-				'msTransform': 'rotate(4deg)',
-				'transform': 'rotate(4deg)'
-				
-			}, options.floatStyle ),
 
-			height = 'height',
-			width = 'width';
+			settings = $.extend( true, {
 
-		if( that.css( 'box-sizing' ) == 'border-box' ){
-			height = 'outerHeight';
-			width = 'outerWidth';
-		}
+				down: fnEmpty,
+				move: fnEmpty,
+				up: fnEmpty,
 
-		return that.on( 'mousedown.DDSort', options.target || 'li', function( e ){
-			//只允许鼠标左键拖动
-			if( e.which != 1 ){
-				return;
-			}
-			
-			//防止表单元素失效
-			var tagName = e.target.tagName.toLowerCase();
-			if( tagName == 'input' || tagName == 'textarea' || tagName == 'select' ){
-				return;
-			}
-
-			var THIS = this,
-				$this = $( THIS ),
-				offset = $this.offset(),
-				disX = e.pageX - offset.left,
-				disY = e.pageY - offset.top,
-			
-				clone = $this.clone()
-					.css( cloneStyle )
-					.css( 'height', $this[ height ]() )
-					.empty(),
-					
-				hasClone = 1,
-
-				//缓存计算
-				thisOuterHeight = $this.outerHeight(),
-				thatOuterHeight = that.outerHeight(),
-
-				//滚动速度
-				upSpeed = thisOuterHeight,
-				downSpeed = thisOuterHeight,
-				maxSpeed = thisOuterHeight * 3;
-			
-			fnDown.call( THIS );
-			
-			$doc.on( 'mousemove.DDSort', function( e ){
-				if( hasClone ){
-					$this.before( clone )
-						.css( 'width', $this[ width ]() )
-						.css( floatStyle )
-						.appendTo( $this.parent() );
-						
-					hasClone = 0;
+				target: 'li',
+				cloneStyle: {
+					'background-color': '#eee'
+				},
+				floatStyle: {
+					//用固定定位可以防止定位父级不是Body的情况的兼容处理，表示不兼容IE6，无妨
+					'position': 'fixed',
+					'box-shadow': '10px 10px 20px 0 #eee',
+					'webkitTransform': 'rotate(4deg)',
+					'mozTransform': 'rotate(4deg)',
+					'msTransform': 'rotate(4deg)',
+					'transform': 'rotate(4deg)'
 				}
 
-				var left = e.pageX - disX,
-					top = e.pageY - disY,
-					
-					prev = clone.prev(),
-					//阻止next为托起的元素$this
-					next = clone.next().not( $this );
+			}, options );
+
+		return this.each(function(){
+
+			var that = $( this ),
+				height = 'height',
+				width = 'width';
+
+			if( that.css( 'box-sizing' ) == 'border-box' ){
+				height = 'outerHeight';
+				width = 'outerWidth';
+			}
+
+			that.on( 'mousedown.DDSort', settings.target, function( e ){
+				//只允许鼠标左键拖动
+				if( e.which != 1 ){
+					return;
+				}
 				
-				$this.css({
-					left: left,
-					top: top
+				//防止表单元素失效
+				var tagName = e.target.tagName.toLowerCase();
+				if( tagName == 'input' || tagName == 'textarea' || tagName == 'select' ){
+					return;
+				}
+				
+				var THIS = this,
+					$this = $( THIS ),
+					offset = $this.offset(),
+					disX = e.pageX - offset.left,
+					disY = e.pageY - offset.top,
+				
+					clone = $this.clone()
+						.css( settings.cloneStyle )
+						.css( 'height', $this[ height ]() )
+						.empty(),
+						
+					hasClone = 1,
+
+					//缓存计算
+					thisOuterHeight = $this.outerHeight(),
+					thatOuterHeight = that.outerHeight(),
+
+					//滚动速度
+					upSpeed = thisOuterHeight,
+					downSpeed = thisOuterHeight,
+					maxSpeed = thisOuterHeight * 3;
+				
+				settings.down.call( THIS );
+				
+				$doc.on( 'mousemove.DDSort', function( e ){
+					if( hasClone ){
+						$this.before( clone )
+							.css( 'width', $this[ width ]() )
+							.css( settings.floatStyle )
+							.appendTo( $this.parent() );
+							
+						hasClone = 0;
+					}
+					
+					var left = e.pageX - disX,
+						top = e.pageY - disY,
+						
+						prev = clone.prev(),
+						next = clone.next().not( $this );
+					
+					$this.css({
+						left: left,
+						top: top
+					});
+					
+					//向上排序
+					if( prev.length && top < prev.offset().top + prev.outerHeight()/2 ){
+							
+						clone.after( prev );
+						
+					//向下排序
+					}else if( next.length && top + thisOuterHeight > next.offset().top + next.outerHeight()/2 ){
+						
+						clone.before( next );
+
+					}
+
+					/**
+					 * 处理滚动条
+					 * that是带着滚动条的元素，这里默认以为that元素是这样的元素（正常情况就是这样），如果使用者事件委托的元素不是这样的元素，那么需要提供接口出来
+					 */
+					var thatScrollTop = that.scrollTop(),
+						thatOffsetTop = that.offset().top,
+						scrollVal;
+					
+					//向上滚动
+					if( top < thatOffsetTop ){
+
+						downSpeed = thisOuterHeight;
+						upSpeed = ++upSpeed > maxSpeed ? maxSpeed : upSpeed;
+						scrollVal = thatScrollTop - upSpeed;
+
+					//向下滚动
+					}else if( top + thisOuterHeight - thatOffsetTop > thatOuterHeight ){
+
+						upSpeed = thisOuterHeight;
+						downSpeed = ++downSpeed > maxSpeed ? maxSpeed : downSpeed;
+						scrollVal = thatScrollTop + downSpeed;
+					}
+
+					that.scrollTop( scrollVal );
+
+					settings.move.call( THIS );
+
+				})
+				.on( 'mouseup.DDSort', function(){
+					
+					$doc.off( 'mousemove.DDSort mouseup.DDSort' );
+					
+					//click的时候也会触发mouseup事件，加上判断阻止这种情况
+					if( !hasClone ){
+						clone.before( $this.removeAttr( 'style' ) ).remove();
+						settings.up.call( THIS );
+					}
 				});
 				
-				//向上排序
-				if( prev.length && top < prev.offset().top + prev.outerHeight()/2 ){
-						
-					clone.after( prev );
-					
-				//向下排序
-				}else if( next.length && top + thisOuterHeight > next.offset().top + next.outerHeight()/2 ){
-					
-					clone.before( next );
-
-				}
-
-				/**
-				 * 处理滚动条
-				 * that是带着滚动条的元素，这里默认以为that元素是这样的元素（正常情况就是这样），如果使用者事件委托的元素不是这样的元素，那么需要提供接口出来
-				 */
-				var thatScrollTop = that.scrollTop(),
-					thatOffsetTop = that.offset().top,
-					scrollVal;
-
-				//向上滚动
-				if( top < thatOffsetTop ){
-
-					downSpeed = thisOuterHeight;
-					upSpeed = ++upSpeed > maxSpeed ? maxSpeed : upSpeed;
-					scrollVal = thatScrollTop - upSpeed;
-
-				//向下滚动
-				}else if( top + thisOuterHeight - thatOffsetTop > thatOuterHeight ){
-
-					upSpeed = thisOuterHeight;
-					downSpeed = ++downSpeed > maxSpeed ? maxSpeed : downSpeed;
-					scrollVal = thatScrollTop + downSpeed;
-				}
-
-				that.scrollTop( scrollVal );
-
-				fnMove.call( THIS );
+				return false;
 			});
-			
-			$doc.on( 'mouseup.DDSort', function(){
-				
-				$doc.off( 'mousemove.DDSort mouseup.DDSort' );
-				
-				//click的时候也会触发mouseup事件，加上判断阻止这种情况
-				if( !hasClone ){
-					clone.before( $this.removeAttr( 'style' ) ).remove();
-					fnUp.call( THIS );
-				}
-			});
-			
-			return false;
 		});
 	};
 
