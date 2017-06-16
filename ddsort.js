25e1036837e7eef3023d93695bc76e829d1f0362
weid <weid@mysoft.com.cn>
2017-06-16 10:19:32 +0800

    [#2] add delay option

--- ddsort.js
+++ ddsort.js
@@ -4,6 +4,7 @@
  * DDSort: drag and drop sorting.
  * @param {Object} options
  *        target[string]:        可选，jQuery 事件委托选择器字符串，默认 li
+ *        delay[number]:         可选，延时拖拽，单位毫秒
  *        cloneStyle[object]:    可选，设置占位符元素的样式
  *        floatStyle[object]:    可选，设置拖动元素的样式
  *        down[function]:        可选，鼠标按下时执行的函数
@@ -16,6 +17,7 @@
         move: $.noop,
         up: $.noop,
         target: 'li',
+        delay: 100,
         cloneStyle: {
             'background-color': '#eee'
         },
@@ -46,6 +48,8 @@
             }
 
             that.on('mousedown.DDSort', settings.target, function (e) {
+
+                var startTime = new Date().getTime();
 
                 // 只允许鼠标左键拖动
                 if (e.which != 1) return;
@@ -82,75 +86,77 @@
                 settings.down.call(self);
 
                 $doc.on('mousemove.DDSort', function (e) {
-                        if (hasClone) {
-                            $this.before(clone)
-                                .css('width', $this[width]())
-                                .css(settings.floatStyle)
-                                .appendTo($this.parent());
 
-                            hasClone = 0;
-                        }
+                    if (new Date().getTime() - startTime < settings.delay) return;
 
-                        var left = e.pageX - disX;
-                        var top = e.pageY - disY;
+                    if (hasClone) {
+                        $this.before(clone)
+                            .css('width', $this[width]())
+                            .css(settings.floatStyle)
+                            .appendTo($this.parent());
 
-                        var prev = clone.prev();
-                        var next = clone.next().not($this);
+                        hasClone = 0;
+                    }
 
-                        $this.css({
-                            left: left - $doc.scrollLeft(),
+                    var left = e.pageX - disX;
+                    var top = e.pageY - disY;
 
-                            // 超出首屏减去页面滚动条高度
-                            top: top - $doc.scrollTop()
-                        });
+                    var prev = clone.prev();
+                    var next = clone.next().not($this);
 
-                        // 向上排序
-                        if (prev.length && top < prev.offset().top + prev.outerHeight() / 2) {
-
-                            clone.after(prev);
-
-                            // 向下排序
-                        } else if (next.length && top + thisOuterHeight > next.offset().top + next.outerHeight() / 2) {
-
-                            clone.before(next);
-
-                        }
-
-                        // 处理滚动条，that 是带着滚动条的元素，这里默认以为 that 元素是这样的元素（正常情况就是这样），
-                        // 如果使用者事件委托的元素不是这样的元素，那么需要提供接口出来
-                        var thatScrollTop = that.scrollTop();
-                        var thatOffsetTop = that.offset().top;
-                        var scrollVal;
-
-                        // 向上滚动
-                        if (top < thatOffsetTop) {
-
-                            downSpeed = thisOuterHeight;
-                            upSpeed = ++upSpeed > maxSpeed ? maxSpeed : upSpeed;
-                            scrollVal = thatScrollTop - upSpeed;
-
-                            // 向下滚动
-                        } else if (top + thisOuterHeight - thatOffsetTop > thatOuterHeight) {
-
-                            upSpeed = thisOuterHeight;
-                            downSpeed = ++downSpeed > maxSpeed ? maxSpeed : downSpeed;
-                            scrollVal = thatScrollTop + downSpeed;
-                        }
-
-                        that.scrollTop(scrollVal);
-
-                        settings.move.call(self, left - $doc.scrollLeft(), top - $doc.scrollTop());
-                    })
-                    .on('mouseup.DDSort', function () {
-
-                        $doc.off('mousemove.DDSort mouseup.DDSort');
-
-                        // click 的时候也会触发 mouseup 事件，加上判断阻止这种情况
-                        if (!hasClone) {
-                            clone.before($this.removeAttr('style')).remove();
-                            settings.up.call(self);
-                        }
+                    // 超出首屏减去页面滚动条高度或宽度
+                    $this.css({
+                        left: left - $doc.scrollLeft(),
+                        top: top - $doc.scrollTop()
                     });
+
+                    // 向上排序
+                    if (prev.length && top < prev.offset().top + prev.outerHeight() / 2) {
+
+                        clone.after(prev);
+
+                        // 向下排序
+                    } else if (next.length && top + thisOuterHeight > next.offset().top + next.outerHeight() / 2) {
+
+                        clone.before(next);
+
+                    }
+
+                    // 处理滚动条，that 是带着滚动条的元素，这里默认以为 that 元素是这样的元素（正常情况就是这样），
+                    // 如果使用者事件委托的元素不是这样的元素，那么需要提供接口出来
+                    var thatScrollTop = that.scrollTop();
+                    var thatOffsetTop = that.offset().top;
+                    var scrollVal;
+
+                    // 向上滚动
+                    if (top < thatOffsetTop) {
+
+                        downSpeed = thisOuterHeight;
+                        upSpeed = ++upSpeed > maxSpeed ? maxSpeed : upSpeed;
+                        scrollVal = thatScrollTop - upSpeed;
+
+                        // 向下滚动
+                    } else if (top + thisOuterHeight - thatOffsetTop > thatOuterHeight) {
+
+                        upSpeed = thisOuterHeight;
+                        downSpeed = ++downSpeed > maxSpeed ? maxSpeed : downSpeed;
+                        scrollVal = thatScrollTop + downSpeed;
+                    }
+
+                    that.scrollTop(scrollVal);
+
+                    settings.move.call(self, left - $doc.scrollLeft(), top - $doc.scrollTop());
+                })
+                .on('mouseup.DDSort', function () {
+
+                    $doc.off('mousemove.DDSort mouseup.DDSort');
+
+                    // click 的时候也会触发 mouseup 事件，加上判断阻止这种情况
+                    if (!hasClone) {
+                        clone.before($this.removeAttr('style')).remove();
+                        settings.up.call(self);
+                    }
+                });
 
                 return false;
             });
